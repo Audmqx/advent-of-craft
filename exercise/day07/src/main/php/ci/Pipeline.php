417fb45b6d17e4a6395d0f94b67008d62f8767f4
MaximPixodeo <77394867+Audmqx@users.noreply.github.com>
2023-12-08 20:21:55 +0100

    tidy run pipeline with extract methods

--- exercise/day07/src/main/php/ci/Pipeline.php
+++ exercise/day07/src/main/php/ci/Pipeline.php
@@ -22,45 +22,105 @@ class Pipeline {
         $testsPassed = false;
         $deploySuccessful = false;
 
+        $testsPassed = $this->isTestsMissings($project, $testsPassed);
+
+        $testsPassed = $this->isTestsPassed($project, $testsPassed);
+
+        $testsPassed = $this->isTestsFailed($project, $testsPassed);
+
+        $deploySuccessful = $this->IsDeploypmentPassed($testsPassed, $project, $deploySuccessful);
+
+        $deploySuccessful = $this->isDeploymentFailed($testsPassed, $project, $deploySuccessful);
+
+        if(!$this->config->sendEmailSummary()) {
+            $this->log->info("Email disabled");
+            return;
+        }
+
+        $this->log->info("Sending email");
+
+        if (!$testsPassed){
+            $this->emailer->send("Tests failed");
+        }
+
+        if (!$deploySuccessful) {
+            $this->emailer->send("Deployment failed");
+        }
+
+        if ($deploySuccessful) {
+            $this->emailer->send("Deployment completed successfully");
+        }
+    }
+
+    /**
+     * @param Project $project
+     * @param bool $testsPassed
+     * @return bool
+     */
+    public function isTestsMissings(Project $project, bool $testsPassed): bool
+    {
         if (!$project->hasTests()) {
             $this->log->info("No tests");
             $testsPassed = true;
         }
+        return $testsPassed;
+    }
 
+    /**
+     * @param Project $project
+     * @param bool $testsPassed
+     * @return bool
+     */
+    public function isTestsPassed(Project $project, bool $testsPassed): bool
+    {
         if ($project->hasTests() && "success" === $project->runTests()) {
             $this->log->info("Tests passed");
             $testsPassed = true;
         }
+        return $testsPassed;
+    }
 
+    /**
+     * @param Project $project
+     * @param bool $testsPassed
+     * @return bool
+     */
+    public function isTestsFailed(Project $project, bool $testsPassed): bool
+    {
         if ($project->hasTests() && "success" != $project->runTests()) {
             $this->log->error("Tests failed");
             $testsPassed = false;
         }
+        return $testsPassed;
+    }
 
-
+    /**
+     * @param bool $testsPassed
+     * @param Project $project
+     * @param bool $deploySuccessful
+     * @return bool
+     */
+    public function IsDeploypmentPassed(bool $testsPassed, Project $project, bool $deploySuccessful): bool
+    {
         if ($testsPassed && "success" === $project->deploy()) {
             $this->log->info("Deployment successful");
             $deploySuccessful = true;
         }
+        return $deploySuccessful;
+    }
 
+    /**
+     * @param bool $testsPassed
+     * @param Project $project
+     * @param bool $deploySuccessful
+     * @return bool
+     */
+    public function isDeploymentFailed(bool $testsPassed, Project $project, bool $deploySuccessful): bool
+    {
         if ($testsPassed && "success" != $project->deploy()) {
             $this->log->error("Deployment failed");
             $deploySuccessful = false;
         }
-
-        if ($this->config->sendEmailSummary()) {
-            $this->log->info("Sending email");
-            if ($testsPassed) {
-                if ($deploySuccessful) {
-                    $this->emailer->send("Deployment completed successfully");
-                } else {
-                    $this->emailer->send("Deployment failed");
-                }
-            } else {
-                $this->emailer->send("Tests failed");
-            }
-        } else {
-            $this->log->info("Email disabled");
-        }
+        return $deploySuccessful;
     }
 }
