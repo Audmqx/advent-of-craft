1b5d2b704d55f9fde98ba14dc25967f7fb1f71d7
MaximPixodeo <77394867+Audmqx@users.noreply.github.com>
2023-12-08 20:46:32 +0100

    setup exercice

---
+++ exercise/day08/src/test/AuthenticationPasswordValidationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/**
+    The password verification program will take a password as an argument and
+    the password will be valid only if all these conditions are correct:
+
+    - Contains at least 8 characters
+    - Contains at least one capital letter
+    - Contains at least one lowercase letter
+    - Contains at least a number
+    - Contains at least a special character in this list `. * # @ $ % &`.
+    - Any other characters are not authorized.
+
+    In this program, the only thing that matters is to know if the password is valid.
+    Not why it is not.
+ */
+
+class AuthenticationPasswordValidationTest
+{
+
+}
