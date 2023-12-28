ff0eda65f5fe3653ea5e0dc861976c2566da5826
MaximPixodeo <77394867+Audmqx@users.noreply.github.com>
2023-12-28 20:32:56 +0100

    add Proprety based testing

--- exercise/day17/src/test/php/games/FizzBuzzTest.php
+++ exercise/day17/src/test/php/games/FizzBuzzTest.php
@@ -4,60 +4,40 @@ namespace Tests\Games;
 
 use Games\FizzBuzz;
 use PHPUnit\Framework\TestCase;
+use Eris\Generators;
 
 class FizzBuzzTest extends TestCase
 {
-    /**
-     * @return array
-     */
-    public static function validInputsProvider()
+    use \Eris\TestTrait;
+
+    public function test_out_of_range_numbers()
     {
-        return [
-            [1, "1"],
-            [67, "67"],
-            [82, "82"],
-            [3, "Fizz"],
-            [66, "Fizz"],
-            [99, "Fizz"],
-            [5, "Buzz"],
-            [50, "Buzz"],
-            [85, "Buzz"],
-            [15, "FizzBuzz"],
-            [30, "FizzBuzz"],
-            [45, "FizzBuzz"],
-        ];
+        $this->forAll(
+            Generators::choose(-100, 200)
+        )
+            ->when( function ($number) {
+                return $number <= 0 || $number > 100;
+            })
+            ->then(function ($number) {
+                $this->assertTrue(FizzBuzz::convert($number)->isEmpty());
+            });
     }
 
-    /**
-     * @return array
-     */
-    public static function invalidInputsProvider()
+    public function test_fizz_Buzz_numbers()
     {
-        return [
-            [0],
-            [-1],
-            [101],
-        ];
-    }
+        $this->forAll(
+            Generators::choose(1, 100)
+        )
+            ->then(function ($number) {
+                $result = FizzBuzz::convert($number)->get();
 
-    /**
-     * @dataProvider validInputsProvider
-     *
-     * @param int $input
-     * @param string $expectedResult
-     */
-    public function testConvertSuccessfullyNumbersBetween1And100(int $input, string $expectedResult)
-    {
-        $this->assertEquals(FizzBuzz::convert($input)->get(), $expectedResult);
-    }
-
-    /**
-     * @dataProvider invalidInputsProvider
-     *
-     * @param int $input
-     */
-    public function testConvertFailsForNumbersOutOfRange(int $input)
-    {
-        $this->assertTrue(FizzBuzz::convert($input)->isEmpty());
+                if ($number % 3 === 0 && $number % 5 === 0) {
+                    $this->assertEquals('FizzBuzz', $result);
+                } elseif ($number % 3 === 0) {
+                    $this->assertEquals('Fizz', $result);
+                } elseif ($number % 5 === 0) {
+                    $this->assertEquals('Buzz', $result);
+                }
+            });
     }
 }
