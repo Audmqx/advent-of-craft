0abbfe3214f7bc0a550fd1bed3207becbb9fe407
MaximPixodeo <77394867+Audmqx@users.noreply.github.com>
2023-12-30 20:22:15 +0100

    php init

---
+++ exercise/day18/src/main/php/lap/ShittyClass.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lap;
+
+class ShittyClass {
+    private const CONSECUTIVE__UNDERSCORES__KILL__READABILITY = "detect me if you can 😬";
+    private const _EXTERNAL_UNDERSCORES_KILL_IT_TOO = "detect me if you can 🚀";
+
+    public function getData() {
+    }
+
+    private function isTrue() {
+        return 42;
+    }
+}
---
+++ exercise/day18/src/test/php/LinguisticAntiPatternTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+class LinguisticAntiPatternTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertEquals(1,1);
+    }
+}
+
