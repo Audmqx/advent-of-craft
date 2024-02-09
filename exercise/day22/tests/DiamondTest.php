331df30ae69235908cf8fb6856cd3474b3bb07ae
MaximPixodeo <77394867+Audmqx@users.noreply.github.com>
2024-02-09 17:12:05 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 9
    test-stats:
        run: 1
        passed: 1
        failed: 0
        skipped: 0
        error: 0
        duration: 6.872ms

--- exercise/day22/tests/DiamondTest.php
+++ exercise/day22/tests/DiamondTest.php
@@ -4,6 +4,15 @@ namespace Test;
 
 use PHPUnit\Framework\TestCase;
 
+
+// The program will take a parameter a letter indicating the depth of the diamond.
+// supplying C as parameter will display:
+//   A
+//  B B
+// C   C
+//  B B
+//   A
+
 class DiamondTest extends TestCase
 {
     public function test()
