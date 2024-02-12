6aa454c0bacec25bf990a5cf0838862786ebcf53
Audmqx <iangaev.m@live.fr>
2024-02-12 15:23:26 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 18
        test: 0
    test-stats:
        run: 1
        passed: 1
        failed: 0
        skipped: 0
        error: 0
        duration: 15.819ms

--- exercise/day22/src/Diamond.php
+++ exercise/day22/src/Diamond.php
@@ -23,20 +23,28 @@ class Diamond
     public function upperSide(): array
     {
         $upperArray = [];
-        for ($i=0; $i < ($this->positionInTheAlphabet() + 1); $i++) { 
+
+        $upperArray = $this->pushACharachter($upperArray);
+
+        for ($i=0; $i < $this->positionInTheAlphabet(); $i++) { 
             array_push($upperArray, $this->addLineToDiamond($this->alphabet()[$i]));
         }
-   
+        
+        $upperArray = $this->pushACharachter($upperArray);
+
         return $upperArray;
     }
 
-
+    private function pushACharachter(array $array): array
+    {
+        return array_push($array, 'A');
+    }
 
     public function addLineToDiamond(string $character): string
     {
         $blanks = '';
 
-        for ($i=0; $i < $this->spacesBetweenChars(); $i++) { 
+        for ($i=0; $i < $this->positionInTheAlphabet(); $i++) { 
             $blanks .= " ";
         }
       
@@ -52,10 +60,6 @@ class Diamond
         }
     }
 
-    public function spacesBetweenChars()
-    {
-        return $this->positionInTheAlphabet();
-    }
 
     private function alphabet(): array
     {
